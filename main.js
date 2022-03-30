33bc10503ed0fad78c6af5e8a0cbaea9ab2d1120
binary-code-978 <93479009+binary-code-978@users.noreply.github.com>
2022-03-30 15:17:25 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,9 +3,7 @@ var SpeechRecognition = window.webkitSpeechRecognition;
 var recognition = new SpeechRecognition();
 
 function start() {
-    document.getElementById("textbox").innerHTML = "";
     recognition.start();
-
 }
 
 recognition.onresult = function (event) {
@@ -15,4 +13,62 @@ recognition.onresult = function (event) {
     console.log(Content);
 
     document.getElementById("textbox").innerHTML = Content;
+}
+
+function speak() {
+    var synth = window.SpeechSynthesis;
+
+    speak_data = "taking your slefie in 5000000000000000000000 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterthis);
+    Webcam.attach(camera);
+
+    setTimeout(function () {
+        img_id = selfie1;
+        take_snapshot();
+        speak_data = "Taking your next Selfie in the next dont even ask me seconds";
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterthis);
+    }, 5000())
+
+    setTimeout(function () {
+        img_id = selfie2;
+        take_snapshot();
+        speak_data = "Taking your next Selfie in the next decade";
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterthis);
+    }, 10000())
+
+    setTimeout(function () {
+        img_id = selfie3;
+        take_snapshot();
+        speak_data = "Taking your next Selfie in the next dont even ask me seconds";
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterthis);
+    }, 15000())
+
+}
+
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+function take_snapshot() {
+
+    webcam.snap(function (data_uri) {
+        if (img_id == "selfie1") {
+            document.getElementById("result1").innerHTML = '<img id="selfie1" src="' + data_uri + '">';
+        }
+        if (img_id == "selfie2") {
+            document.getElementById("result2").innerHTML = '<img id="selfie2" src="' + data_uri + '">';
+        }
+        if (img_id == "selfie3") {
+            document.getElementById("result3").innerHTML = '<img id="selfie3" src="' + data_uri + '">';
+        }
+    });
 }
